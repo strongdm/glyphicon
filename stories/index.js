4844ec66735a352cac4e294f1a79673c338eebed
Britt Crawford <britt.v.crawford@gmail.com>
2019-05-09 09:44:50 -0700

    add more stories

--- stories/index.js
+++ stories/index.js
@@ -13,3 +13,24 @@ storiesOf('Glyphicon', module)
         The info sign <Glyphicon glyph='info-sign' />
     </div>
   ))
+  .add('danger variant', () => (
+    <div>
+        The info sign <Glyphicon glyph='info-sign' bsStyle='danger' />
+    </div>
+  ))
+  .add('with a custom CSS class', () => (
+    <div>
+      <style dangerouslySetInnerHTML={{__html: '.bananas { color: yellow; background-color: black}'}} />
+        The info sign <Glyphicon glyph='info-sign' className='bananas' />
+    </div>
+  ))
+  .add('with a style attribute', () => (
+    <div>
+        The info sign <Glyphicon glyph='info-sign' style={{color: 'purple'}} />
+    </div>
+  ))
+  .add('large size', () => (
+    <div>
+        The info sign <Glyphicon glyph='info-sign' bsSize='x-large' />
+    </div>
+  ))
